c3cd59764d8fe9c5931aac209d7180ccb3af8f75
Vulpon <danieldenardo1@gmail.com>
2019-05-30 16:40:32 -0300

    wtf

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@
 const Discord = require('discord.js');
 const tokenfile = require('./token.json')
 const botconfig = require('./botconfig.json');
-const glob = require("glob");
-const path = require("path");
+const fs = require("fs");
+const queue = new Map();
+
 
 // Declare Bot
 const bot = new Discord.Client({disableEveryone: true});
@@ -11,17 +12,29 @@ bot.commands = new Discord.Collection();
 
 
 // Filesystem + Reading Commands
-
-glob.sync("./commands/**/*.js").forEach(function(file){
-    let props = require(path.resolve(file))
-    console.log(`${props.help.name} loaded!`);
-    bot.commands.set(props.help.name, props)
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.log(err);
+    
+    let jsfile = files.filter(f => f.split(".").pop() === "js"); 
+    
+    // If no file could be found
+    if(jsfile.length <= 0) {
+        console.log("Couldn't find commands.");
+        return;
+    }
+    
+    // Logs & loads commands
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props)
+    });
 });
 
 // Initialize Bot
 bot.on('ready', async () => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setPresence({game:{name: "GameDev", type: 0}});
+    bot.user.setPresence({game:{name: "Discord Administration", type: 0}});
 });
 
 bot.on('message', async message =>  {
@@ -34,14 +47,23 @@ bot.on('message', async message =>  {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(0); // Slice the command from the message
+    let serverQueue = queue.get(message.guild.id);
     
     // Get the Command Files
     if(!message.content.startsWith(botconfig.prefix)) return; 
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     
     // Run the Commands
-    if (commandfile) commandfile.run(bot, message, args);
+    if (commandfile) commandfile.run(bot, message, args, queue, serverQueue);
 
+});
+
+bot.on('disconnect', () => {
+    if(message.guild.me.voiceChannel) {
+        queue.delete(message.guild.id);
+        message.guild.me.voiceChannel.leave();
+        console.log('I just disconnected, making sure you know, I will reconnect now...');
+    }
 });
 
 bot.login(tokenfile.token);
